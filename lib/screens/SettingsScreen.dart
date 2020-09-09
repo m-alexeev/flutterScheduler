af4c04f10e4f4dda0c64abef419003558514fe93
B0uncyKnight <mishalxv35@gmail.com>
2020-09-08 22:41:46 -0700

    Working on adding Profile Picture

--- lib/screens/SettingsScreen.dart
+++ lib/screens/SettingsScreen.dart
@@ -1,8 +1,10 @@
+import 'dart:io';
+
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart' as auth;
 import 'package:flutter/material.dart';
-import 'package:flutter_app/models/User.dart';
 import 'package:flutter_app/screens/auth/LoginScreen.dart';
+import 'package:image_picker/image_picker.dart';
 
 class UserScreen extends StatefulWidget {
   @override
@@ -10,8 +12,18 @@ class UserScreen extends StatefulWidget {
 }
 
 class _UserScreenState extends State<UserScreen> {
-
   var user = auth.FirebaseAuth.instance.currentUser;
+
+  final picker = ImagePicker();
+  File _userImage;
+  Future pickImageFromGallery() async {
+    final imageFile = await picker.getImage(source: ImageSource.gallery);
+    setState(() {
+      _userImage = File(imageFile.path);
+    });
+  }
+
+
 
   @override
   Widget build(BuildContext context) {
@@ -22,9 +34,21 @@ class _UserScreenState extends State<UserScreen> {
       body: Scaffold(
         body: Column(
           children: [
+            CircleAvatar(
+              radius: 50,
+              backgroundImage: _userImage == null
+                  ? Image.network("https://moonvillageassociation.org/wp-content/uploads/2018/06/default-profile-picture1.jpg").image
+                  : Image.file(_userImage).image,
+            ),
+            FlatButton.icon(
+              label: Text("Upload Picture"),
+              icon: Icon(Icons.image),
+              color: Colors.blueAccent,
+              onPressed: () => pickImageFromGallery(),
+            ),
             FlatButton(
               child: Text("Sign Out"),
-              onPressed: () async =>_signOut(),
+              onPressed: () async => _signOut(),
             ),
           ],
         ),
@@ -32,7 +56,7 @@ class _UserScreenState extends State<UserScreen> {
     );
   }
 
-  void _signOut() async{
+  void _signOut() async {
     auth.User user = auth.FirebaseAuth.instance.currentUser;
     if (user != null) {
       try {
@@ -40,12 +64,9 @@ class _UserScreenState extends State<UserScreen> {
           Navigator.pushReplacement(context,
               new MaterialPageRoute(builder: (context) => new LoginScreen()));
         });
-      }
-      catch(e){
+      } catch (e) {
         print(e.toString());
       }
     }
   }
-
-
 }
